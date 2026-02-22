6c8aa117dcea8ab17a2c65d34a8baf1d5ee30790
wootaiklee <fined000@naver.com>
2026-02-22 15:29:12 +0900

    feat: integrate expo-blur for enhanced UI and update navigation
    - Added expo-blur dependency to improve visual effects in the home screen.
    - Refactored home screen layout to include a header with a logo and conditional login button.
    - Updated tab navigation to display based on user authentication state.

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,32 +1,88 @@
 import { usePathname, useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet, Image } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { BlurView } from "expo-blur";
 
 export default function Index() {
   const router = useRouter();
   const pathname = usePathname();
-
+  const insects = useSafeAreaInsets();
+  const isLoggedIn = true;
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={
+        (styles.container,
+        {
+          paddingTop: insects.top,
+          paddingBottom: insects.bottom,
+        })
+      }
     >
-      <View>
-        <TouchableOpacity onPress={() => router.push("/")}>
-          <Text style={{ color: pathname === "/" ? "red" : "black" }}>
-            For you
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <TouchableOpacity onPress={() => router.push("/following")}>
-          <Text style={{ color: pathname === "/" ? "black" : "red" }}>
-            Following
-          </Text>
-        </TouchableOpacity>
-      </View>
+      <BlurView style={styles.header}>
+        <Image
+          source={require("@/assets/images/react-logo.png")}
+          style={styles.headerLogo}
+        />
+        {!isLoggedIn && (
+          <TouchableOpacity
+            style={styles.loginButton}
+            onPress={() => router.navigate("/login")}
+          >
+            <Text style={styles.loginButtonText}>로그인</Text>
+          </TouchableOpacity>
+        )}
+      </BlurView>
+      {isLoggedIn && (
+        <View style={styles.tabContainer}>
+          <View style={styles.tab}>
+            <TouchableOpacity onPress={() => router.push("/")}>
+              <Text style={{ color: pathname === "/" ? "red" : "black" }}>
+                For you
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.tab}>
+            <TouchableOpacity onPress={() => router.push("/following")}>
+              <Text style={{ color: pathname === "/" ? "black" : "red" }}>
+                Following
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tabContainer: {
+    flexDirection: "row",
+  },
+  tab: {
+    flex: 1,
+  },
+  header: {
+    alignItems: "center",
+  },
+  headerLogo: {
+    width: 42,
+    height: 42,
+  },
+  loginButton: {
+    position: "absolute",
+    right: 20,
+    top: 0,
+    borderWidth: 1,
+    backgroundColor: "black",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    padding: 10,
+    borderRadius: 10,
+  },
+  loginButtonText: {
+    color: "white",
+  },
+});
