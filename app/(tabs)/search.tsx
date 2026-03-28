32e729ba0bf58f5d157c00466a1c6c6fe5e91ceb
wootaiklee <fined000@naver.com>
2026-03-28 13:55:38 +0900

    feat: enhance search screen with user context and responsive design
    - Refactored SearchScreen to include user authentication context and conditional rendering for the side menu.
    - Implemented responsive design using SafeAreaInsets and color scheme detection for improved UI.
    - Added a search bar with input handling and integrated Ionicons for enhanced user interaction.
    - Introduced styles for light and dark modes to ensure visual consistency across different themes.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,154 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Pressable, TextInput, useColorScheme, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+import { useContext, useState } from "react";
+import { AuthContext } from "../_layout";
+import { Ionicons } from "@expo/vector-icons";
+import { Image } from "expo-image";
+import SideMenu from "@/components/side-menu";
 
 export default function SearchScreen() {
+  const { user } = useContext(AuthContext);
+  const isLoggedIn = !!user;
+  const insets = useSafeAreaInsets();
+  const colorScheme = useColorScheme();
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View>
-        <Text>Search</Text>
+    <View
+      style={[
+        styles.container,
+        {
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+        },
+        colorScheme === "dark" ? styles.containerDark : styles.containerLight,
+      ]}
+    >
+      <View
+        style={[
+          styles.header,
+          colorScheme === "dark" ? styles.headerDark : styles.headerLight,
+        ]}
+      >
+        {isLoggedIn && (
+          <Pressable
+            style={styles.menuButton}
+            onPress={() => {
+              setIsSideMenuOpen(true);
+            }}
+          >
+            <Ionicons
+              name="menu"
+              size={24}
+              color={colorScheme === "dark" ? "gray" : "black"}
+            />
+          </Pressable>
+        )}
+        <Image
+          source={require("../../assets/images/react-logo.png")}
+          style={styles.logo}
+        />
+        <SideMenu
+          isVisible={isSideMenuOpen}
+          onClose={() => setIsSideMenuOpen(false)}
+        />
       </View>
-    </SafeAreaView>
+      <View
+        style={[
+          styles.searchBar,
+          colorScheme === "dark" ? styles.searchBarDark : styles.searchBarLight,
+        ]}
+      >
+        <Ionicons
+          name="search"
+          size={24}
+          color={colorScheme === "dark" ? "gray" : "black"}
+        />
+        <TextInput
+          style={[
+            styles.searchInput,
+            colorScheme === "dark"
+              ? styles.searchInputDark
+              : styles.searchInputLight,
+          ]}
+          placeholder="Search"
+          placeholderTextColor={colorScheme === "dark" ? "gray" : "black"}
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+        />
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  logo: {
+    width: 32,
+    height: 32,
+  },
+  containerLight: {
+    backgroundColor: "white",
+  },
+  containerDark: {
+    backgroundColor: "#101010",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+  },
+  headerLight: {
+    backgroundColor: "white",
+  },
+  headerDark: {
+    backgroundColor: "#101010",
+  },
+  menuButton: {
+    position: "absolute",
+    left: 16,
+  },
+  searchBarArea: {
+    flexDirection: "row",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+  },
+  searchBarAreaLight: {
+    backgroundColor: "white",
+  },
+  searchBarAreaDark: {
+    backgroundColor: "#202020",
+  },
+  searchBar: {
+    width: "100%",
+    height: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 20,
+    paddingHorizontal: 30,
+  },
+  searchBarLight: {
+    backgroundColor: "white",
+  },
+  searchBarDark: {
+    backgroundColor: "black",
+    color: "white",
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#aaa",
+  },
+  searchInput: {
+    marginLeft: 10,
+  },
+  searchInputLight: {
+    color: "black",
+  },
+  searchInputDark: {
+    color: "white",
+  },
+});
